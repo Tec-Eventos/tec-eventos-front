e50b33fcb0fcbde747192541982a488e9c36f068
joao_aguiar <joaopedroaguiar0707@gmail.com>
2023-05-05 18:23:54 -0300

    Terminei os TextBox e estou arrumando o radio

--- tec_eventos/lib/paginas/acesso/login.dart
+++ tec_eventos/lib/paginas/acesso/login.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -11,6 +12,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool selectedValue = true;
+
   @override
   Widget build(BuildContext context) {
     var i = 300;
@@ -39,50 +42,97 @@ class _LoginPageState extends State<LoginPage> {
                   )
                 ),
 
-                SizedBox(height: 30,),
+                TextBox("E-mail"),
+
+
+                TextBox("Usuário"),
+
+                TextBox("Senha"),
 
                 Padding(
-                  padding: 
-                    const EdgeInsets.only(top: 30, left:40, right: 40),
-                    // const EdgeInsets.symmetric(horizontal: 40.0),  
-                  child: Container(
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
-                         color: Colors.grey[300]!,
-                          offset:const Offset(10, 10),
-                          blurRadius: 6,
-                          spreadRadius: -5,
-                        )
-                      ]
+                  padding: const EdgeInsets.only(top: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Transform.scale(
+                          scale: 1.5,
+                          child: Radio(
+                            value: 1,
+                            groupValue: selectedValue, 
+                            onChanged: (value) => setState(() => selectedValue = !selectedValue),
+                          ),
+                         ),
+                        Text('Lembrar-se de mim',
+                          textAlign: TextAlign.center,
+                          style: 
+                          TextStyle(
+                            color: Color(0xffA69F9F),
+                            fontSize: 15.0,
+                          )
+                        ),
+                      ],
                     ),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        isDense: true,
-                        hintText: hint, 
-                        border: InputBorder.none,
-                        contentPadding: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 30.0),
-                        // focusedBorder: OutlineInputBorder(
-                        //   borderSide: const BorderSide(width: 1, color: Color(0xffE9E3E3)),
-                        //   borderRadius: BorderRadius.circular(27),
-                        // ),
-                        // enabledBorder: OutlineInputBorder(
-                        //   borderSide: const BorderSide(width: 1, color: Color(0xffE9E3E3)),
-                        //   borderRadius: BorderRadius.circular(27),
-                        // ),
-                      ),                    
-                    ),
-                  ),
-                ),
+                ),   
 
-                
+                Text(
+                  'Esqueceu sua senha?',
+                  textAlign: TextAlign.center,
+                  style: 
+                    TextStyle(
+                            color: Color(0xff45B0F0),
+                            fontSize: 15.0,
+                    ),
+                )
+
               ],       
             ),
-
       ),
     );
   }
+}
+
+
+TextBox(String hint){
+  return Padding(
+      padding: 
+        const EdgeInsets.only(top: 30, left:40, right: 40),
+        // const EdgeInsets.symmetric(horizontal: 40.0),  
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[300]!,
+              offset:const Offset(10, 10),
+              blurRadius: 6,
+              spreadRadius: -5,
+            )
+          ]
+        ),
+        child: TextFormField(
+          decoration: InputDecoration(
+            isDense: true,
+            hintText: hint, 
+            hintStyle: 
+              TextStyle(
+                color: Color(0xffA69F9F),
+              ),
+            border: InputBorder.none,
+            // contentPadding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+            contentPadding: const EdgeInsets.only(top: 18.0, right: 20, bottom: 18.0, left: 40),
+            // focusedBorder: OutlineInputBorder(
+            //   borderSide: const BorderSide(width: 1, color: Color(0xffE9E3E3)),
+            //   borderRadius: BorderRadius.circular(27),
+            // ),
+            // enabledBorder: OutlineInputBorder(
+            //   borderSide: const BorderSide(width: 1, color: Color(0xffE9E3E3)),
+            //   borderRadius: BorderRadius.circular(27),
+            // ),
+            // Comandos para mudar a cor do botão, antes e durante quando for escrever
+          ),                    
+        ),
+      ),
+    );
 }
