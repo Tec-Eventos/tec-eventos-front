ef5b0cf5be7973ee00a47f4c1295e4b3c8fb1a75
fgabrielmorais <gabrielgamer.aleatoriow@gmail.com>
2023-05-30 16:49:41 -0300

    onBoarding quase feita
    Hoje refiz a OnBoarding que tinha sido alterada

--- tec_eventos/lib/paginas/onBoarding/onboarding.dart
+++ tec_eventos/lib/paginas/onBoarding/onboarding.dart
@@ -39,55 +39,74 @@ class _OnBoardingState extends State<OnBoarding> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          //função para a transição de uma etapa da página para a outra
-          Expanded(
-            child: PageView.builder(
-                itemCount: demo_data.length,
-                controller: _pageController,
-                onPageChanged: (index) {
-                  setState(() {
-                    _pageIndex = index;
-                  });
-                },
-                itemBuilder: (context, index) => TelaBoarding(
-                    demo_data[index].image,
-                    demo_data[index].titulo,
-                    demo_data[index].descricao,
-                    context)),
-          ),
+      body: PageView.builder(
+          itemCount: demo_data.length,
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _pageIndex = index;
+            });
+          },
+          itemBuilder: (context, index) => TelaBoarding(demo_data[index].image,
+              demo_data[index].titulo, demo_data[index].descricao, context)),
 
-          //se a função estiver na terceira etapa, aparece o botão de começar
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: _pageIndex == 3
-                //botão de começar, para entrar na página de login
-                ? const BotaoComecar()
+      //parte de baixo, com os botões e etapas passadas
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: _pageIndex == 3
+            //botão de começar, para entrar na página de login
+            ? const BotaoComecar()
 
-                //parte inferior do onBoarding mostrando em que parte o usuário está
-                : Row(
-                    children: [
-                      //as bolinhas para indicar a etapa, lista gerada para passar a informação para a classe DotIndicator
-                      ...List.generate(
-                          demo_data.length,
-                          (index) => Padding(
-                                padding: const EdgeInsets.only(right: 4),
-                                child: DotIndicator(
-                                  isActive: index == _pageIndex,
-                                ),
-                              )),
+            //parte inferior do onBoarding mostrando em que parte o usuário está
+            : Row(
+                children: [
+                  //as bolinhas para indicar a etapa, lista gerada para passar a informação para a classe DotIndicator
+                  ...List.generate(
+                      demo_data.length,
+                      (index) => Padding(
+                            padding: const EdgeInsets.only(right: 4),
+                            child: DotIndicator(
+                              isActive: index == _pageIndex,
+                            ),
+                          )),
 
-                      //espaçamento
-                      const Spacer(),
+                  //espaçamento
+                  const Spacer(),
 
-                      //botão de passar para a próxima página
-                      const Botao_Proximo(),
-                    ],
-                  ),
-          ),
-        ],
+                  //botão de passar para a próxima página
+                  const Botao_Proximo(),
+                ],
+              ),
       ),
+    );
+
+    //se a função estiver na terceira etapa, aparece o botão de começar
+    Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: _pageIndex == 3
+          //botão de começar, para entrar na página de login
+          ? const BotaoComecar()
+
+          //parte inferior do onBoarding mostrando em que parte o usuário está
+          : Row(
+              children: [
+                //as bolinhas para indicar a etapa, lista gerada para passar a informação para a classe DotIndicator
+                ...List.generate(
+                    demo_data.length,
+                    (index) => Padding(
+                          padding: const EdgeInsets.only(right: 4),
+                          child: DotIndicator(
+                            isActive: index == _pageIndex,
+                          ),
+                        )),
+
+                //espaçamento
+                const Spacer(),
+
+                //botão de passar para a próxima página
+                const Botao_Proximo(),
+              ],
+            ),
     );
   }
 }
@@ -103,65 +122,69 @@ class Onboard {
 final List<Onboard> demo_data = [
   //tutorial 1
   Onboard(
-    image: './assets/t!e_logo.png',
-    titulo: 'OnBoarding1',
-    descricao:
-        'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
+    image: 'assets/onBoarding/conecte.png',
+    titulo: 'Conecte-se',
+    descricao: 'Conecte-se ao mundo acadêmico da melhor maneira possível',
   ),
-
   //tutorial 2
   Onboard(
-    image: './assets/t!e_logo.png',
-    titulo: 'OnBoarding2',
-    descricao:
-        'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
+    image: 'assets/onBoarding/busque.png',
+    titulo: 'Busque',
+    descricao: 'Ache eventos que são do seu interesse.',
   ),
 
   //tutorial 3
   Onboard(
-    image: './assets/t!e_logo.png',
-    titulo: 'OnBoarding3',
-    descricao:
-        'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
+    image: 'assets/onBoarding/pague.png',
+    titulo: 'Pague',
+    descricao: 'Conecte-se ao mundo acadêmico da melhor maneira possível',
   ),
 
   //tutorial 4
   Onboard(
-    image: './assets/t!e_logo.png',
-    titulo: 'OnBoarding4',
-    descricao:
-        'Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum',
-  )
+    image: 'assets/onBoarding/comecar.png',
+    titulo: '',
+    descricao: '',
+  ),
 ];
 
 //classe para as informações de cada tutorial
 TelaBoarding(String image, String titulo, String descricao, context) {
-  return Center(
-    child: ListView(
-      scrollDirection: Axis.vertical,
-      children: [
-        //imagem
-        Image.asset(image, height: MediaQuery.of(context).size.height / 2.5),
-        //título
-        Text(
-          titulo,
-          style: GoogleFonts.cabin(fontSize: 35),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height / 11.5,
-        ),
-
-        //descricao
-        Text(
-          descricao,
-          textAlign: TextAlign.center,
-          style: GoogleFonts.raleway(
-            fontSize: 20.0,
+  return ListView(
+    children: [
+      Column(
+        children: [
+          Image.asset(image, fit: BoxFit.fill, width: double.infinity),
+          Text(
+            titulo,
+            style: GoogleFonts.cabin(
+                color: Cores.Azul42A5F5,
+                fontWeight: FontWeight.bold,
+                fontSize: 30),
+            textAlign: TextAlign.center,
           ),
-        ),
-      ],
-    ),
+
+          Container(
+            height: 1.0,
+            width: 174.03,
+            color: Cores.Azul42A5F5,
+          ),
+          Text(
+            descricao,
+            style: GoogleFonts.raleway(
+                fontWeight: FontWeight.w600,
+                fontSize: 23,
+                shadows: [
+                  Shadow(
+                      color: Colors.black.withOpacity(0.3),
+                      offset: const Offset(1, 2),
+                      blurRadius: 7),
+                ]),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      )
+    ],
   );
 }
 
@@ -229,7 +252,8 @@ class _BotaoComecarState extends State<BotaoComecar> {
             Navigator.push(
                 context,
                 PageTransition(
-                    child: const AllPages(), type: PageTransitionType.rightToLeft));
+                    child: const AllPages(),
+                    type: PageTransitionType.rightToLeft));
           },
           child: Text(
             'COMEÇAR',
