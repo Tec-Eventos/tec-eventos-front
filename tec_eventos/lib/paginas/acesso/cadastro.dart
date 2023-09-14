2e7102035bbed977229f6b2966ef1d47049227a2
fgabrielmorais <gabrielgamer.aleatoriow@gmail.com>
2023-09-14 10:52:58 -0300

    v68
    página de cadastro do aluno

--- tec_eventos/lib/paginas/acesso/cadastro.dart
+++ tec_eventos/lib/paginas/acesso/cadastro.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:tec_eventos/cores.dart';
+import 'package:tec_eventos/fontes.dart';
+import 'package:tec_eventos/paginas/acesso/login.dart';
 import 'package:tec_eventos/paginas/all_pages.dart';
 
 class Cadastro extends StatefulWidget {
@@ -43,223 +45,19 @@ class _CadastroState extends State<Cadastro> {
               fontSize: 28,
             ),
           ),
-          Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            CaixaTexto(icon: Icons.email_outlined, hint: "E-mail"),
-            CaixaTexto(icon: Icons.person_2_outlined, hint: "Usuário"),
-            CaixaTexto(icon: Icons.lock_outlined, hint: "Senha")
-          ]),
-          ListTile(
-            titleAlignment: ListTileTitleAlignment.center,
-            autofocus: true,
-            dense: true,
-            leading: Checkbox(
-              shape: const CircleBorder(),
-              activeColor: Cores.azul42A5F5,
-              value: isChecked,
-              onChanged: (value) {
-                isChecked = !isChecked;
-                setState(() {});
-              },
-            ),
-            title: Text(
-              "Lembrar-se de mim",
-              style: GoogleFonts.inter(
-                fontSize: 15,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: GestureDetector(
-              onTap: () {
-                //a tela que aparecerá a respeito do usuário esquecer a senha
-                showModalBottomSheet(
-                    context: context,
-                    isScrollControlled: true,
-                    // padding: EdgeIn
-                    shape: const RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.vertical(top: Radius.circular(25))),
-                    builder: (context) {
-                      return Padding(
-                        padding: MediaQuery.of(context).viewInsets,
-                        child: SizedBox(
-                          height: 371,
-                          child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Esqueceu sua senha?",
-                                    style: GoogleFonts.inter(
-                                      fontSize: 23,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 20),
-                                  Text(
-                                    "Informe seu e-mail para que possamos lhe enviar um código de confirmação",
-                                    style: GoogleFonts.inter(
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 50),
-                                  Text(
-                                    'Email',
-                                    style: GoogleFonts.inter(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 10),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius:
-                                              BorderRadius.circular(30),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: Colors.grey[300]!,
-                                              offset: const Offset(10, 10),
-                                              blurRadius: 6,
-                                              spreadRadius: -5,
-                                            )
-                                          ]),
-                                      child: TextFormField(
-                                        decoration: const InputDecoration(
-                                          prefixIcon:
-                                              Icon(Icons.email_outlined),
-                                          isDense: true,
-                                          hintText: "Email",
-                                          hintStyle: TextStyle(
-                                              color: Color(0xffA69F9F)),
-                                          border: InputBorder.none,
-                                          contentPadding: EdgeInsets.only(
-                                              top: 18.0,
-                                              right: 20,
-                                              bottom: 18.0,
-                                              left: 40),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 20),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 30),
-                                    child: Container(
-                                      width: 282,
-                                      height: 52,
-                                      decoration: BoxDecoration(
-                                          gradient: LinearGradient(
-                                              colors: [
-                                                Cores.azul47BBEC,
-                                                Cores.azul42A5F5,
-                                              ],
-                                              begin: Alignment.topLeft,
-                                              end: Alignment.bottomRight),
-                                          borderRadius:
-                                              BorderRadius.circular(20)),
-                                      child: Center(
-                                        child: Text(
-                                          "Enviar",
-                                          style: GoogleFonts.raleway(
-                                              fontSize: 28,
-                                              fontWeight: FontWeight.bold,
-                                              color: Cores.branco),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ]),
-                          ),
-                        ),
-                      );
-                    });
-              },
-              child: Text(
-                'Esqueceu a senha?',
-                style: GoogleFonts.inter(
-                  color: Cores.azul45B0F0,
-                  fontWeight: FontWeight.w400,
-                ),
-                textAlign: TextAlign.start,
-              ),
-            ),
-          ),
-          const SizedBox(height: 30),
-          Text(
-            "Faça login por outras mídias sociais",
-            style: GoogleFonts.inter(
-              fontSize: 15,
-              color: Cores.cinza,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          const Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Column(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Cores.cinza,
-                      child: Image.asset('assets/acesso/microsoft.png'),
-                    ),
-                    Text(
-                      'Microsoft',
-                      style: GoogleFonts.inter(
-                        fontSize: 15,
-                        color: Cores.cinza,
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  width: 50,
-                ),
-                Column(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Cores.cinza,
-                      child: Image.asset(
-                        'assets/acesso/google.png',
-                      ),
-                    ),
-                    Text(
-                      'Google',
-                      style: GoogleFonts.inter(
-                        fontSize: 15,
-                        color: Cores.cinza,
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  width: 50,
-                ),
-                Column(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Cores.cinza,
-                      child: Image.asset('assets/acesso/convidado.png'),
-                    ),
-                    Text(
-                      'Convidado',
-                      style: GoogleFonts.inter(
-                        fontSize: 15,
-                        color: Cores.cinza,
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
+                CaixaTexto(icon: Icons.person_2_outlined, hint: "Nome"),
+                CaixaTexto(icon: Icons.email_outlined, hint: "E-mail"),
+                CaixaTexto(icon: Icons.phone, hint: "Telefone"),
+                CaixaTextoDataNascimento(),
+                CaixaTexto(icon: Icons.email_outlined, hint: "E-mail"),
+                CaixaTextoSenha(icon: Icons.lock_outlined, hint: "Senha"),
+                CaixaTextoSenha(
+                    icon: Icons.lock_outlined, hint: "Confirme a senha"),
+              ]),
+          const SizedBox(height: 30),
           GestureDetector(
             onTap: () {
               Navigator.push(
@@ -297,12 +95,10 @@ class _CadastroState extends State<Cadastro> {
             padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
             child: GestureDetector(
               onTap: () {
-                PageTransition(
-                    child: AllPages(paginaAtual: 0),
-                    type: PageTransitionType.rightToLeft);
+                  Navigator.pop(context);
               },
               child: Text(
-                'Não tem uma conta? Cadastre-se',
+                'Já tem uma conta? Faça o login',
                 style: GoogleFonts.inter(
                   color: Cores.azul45B0F0,
                   fontSize: 18,
@@ -356,6 +152,141 @@ class _CaixaTextoState extends State<CaixaTexto> {
                 top: 18.0, right: 20, bottom: 18.0, left: 40),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CaixaTextoSenha extends StatelessWidget {
+  const CaixaTextoSenha({Key? key, required this.icon, required this.hint})
+      : super(key: key);
+  final IconData icon;
+  final String hint;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey[300]!,
+                offset: const Offset(10, 10),
+                blurRadius: 6,
+                spreadRadius: -5,
+              )
+            ]),
+        child: TextFormField(
+          obscureText: true,
+          decoration: InputDecoration(
+            prefixIcon: Icon(icon),
+            isDense: true,
+            hintText: hint,
+            hintStyle:
+                TextStyle(color: Color(0xffA69F9F), fontFamily: Fontes.inter),
+            border: InputBorder.none,
+            contentPadding: const EdgeInsets.only(
+                top: 18.0, right: 20, bottom: 18.0, left: 40),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CaixaTextoDataNascimento extends StatelessWidget {
+  const CaixaTextoDataNascimento({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        children: [
+          Container(
+            width: 72,
+            height: 52,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[300]!,
+                    offset: const Offset(10, 10),
+                    blurRadius: 6,
+                    spreadRadius: -5,
+                  )
+                ]),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "dia",
+                isDense: true,
+                hintStyle: TextStyle(
+                    color: Color(0xffA69F9F), fontFamily: Fontes.inter),
+                border: InputBorder.none,
+                contentPadding:
+                    const EdgeInsets.only(top: 18.0, bottom: 18.0, left: 20),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            width: 72,
+            height: 52,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[300]!,
+                    offset: const Offset(10, 10),
+                    blurRadius: 6,
+                    spreadRadius: -5,
+                  )
+                ]),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "mês",
+                isDense: true,
+                hintStyle: TextStyle(
+                    color: Color(0xffA69F9F), fontFamily: Fontes.inter),
+                border: InputBorder.none,
+                contentPadding:
+                const EdgeInsets.only(top: 18.0, bottom: 18.0, left: 20),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            width: 115,
+            height: 52,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[300]!,
+                    offset: const Offset(10, 10),
+                    blurRadius: 6,
+                    spreadRadius: -5,
+                  )
+                ]),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "ano",
+                isDense: true,
+                hintStyle: TextStyle(
+                    color: Color(0xffA69F9F), fontFamily: Fontes.inter),
+                border: InputBorder.none,
+                contentPadding:
+                const EdgeInsets.only(top: 18.0, bottom: 18.0, left: 30),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
